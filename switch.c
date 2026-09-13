c432f3057b6908995d245de4f4f3330b89973ea8
zakerasultanamanna-source <zakerasultanamanna@gmail.com>
2026-09-13 21:59:03 +0600

    fifth commit

---
+++ switch.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main ()
+{
+    int day=4;
+    switch(day)
+    {
+        case 1:
+        printf("monday");
+        break;
+        case 2:
+        printf("tuesday");
+        break;
+        case 3:
+        printf("wednessday");
+        break;
+        case 4:
+        printf("thursday");
+        break;
+        case 5:
+        printf("friday");
+        break;
+
+    }
+    return 0;
+}
